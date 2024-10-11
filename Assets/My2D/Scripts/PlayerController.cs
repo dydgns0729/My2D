021560a2c214c2b903f73317b6b22569c5edcd5e
toneed <dydgns0729@gmail.com>
2024-10-11 17:31:02 +0900

    1. 플레이어 대기, 걷기, 뛰기 애니메이션 2. 플레이어 이동방향에 따른 좌우 반전 3. 플레이어 점프 애니 셋팅

--- Assets/My2D/Scripts/PlayerController.cs
+++ Assets/My2D/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,15 +7,62 @@ namespace My2D
     {
         #region Variables
         Rigidbody2D rb2d;
+        Animator animator;
+
         //플레이어 이동 속도
         [SerializeField] float walkSpeed = 4f;
         //플레이어 이동과 관련된 입력값
         Vector2 inputMove;
+
+        //걷기
+        [SerializeField] private bool isMove;
+        public bool IsMove
+        {
+            get { return isMove; }
+            set
+            {
+                isMove = value;
+                animator.SetBool(AnimationString.IsMove, value);
+            }
+        }
+
+        //뛰기
+        [SerializeField] private bool isRun;
+        public bool IsRun
+        {
+            get { return isRun; }
+            set
+            {
+                isRun = value;
+                animator.SetBool(AnimationString.IsRun, value);
+            }
+        }
+
+        //좌우 반전(스케일 컨트롤)
+        [SerializeField] private bool isFacingRight;
+        public bool IsFacingRight
+        {
+            get { return isFacingRight; }
+            set
+            {
+                //반전
+                if (isFacingRight != value)
+                {
+                    transform.localScale *= new Vector2(-1, 1);
+                }
+                isFacingRight = value;
+            }
+        }
         #endregion
         void Awake()
         {
             //참조
             rb2d = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            //초기화
+            isMove = false;
+            isRun = false;
+            isFacingRight = true;
         }
 
         private void FixedUpdate()
@@ -27,13 +72,40 @@ namespace My2D
 
         }
 
+        //바라보는 방향으로 전환(스케일 컨트롤)
+        void SetFacingDirection(Vector2 moveInput)
+        {
+            if (moveInput.x > 0f && !IsFacingRight)
+            {
+                //오른쪽을 바라본다
+                IsFacingRight = true;
+            }
+            else if (moveInput.x < 0f && IsFacingRight)
+            {
+                //왼쪽을 바라본다
+                IsFacingRight = false;
+            }
+        }
+
         public void OnMove(InputAction.CallbackContext context)
         {
             inputMove = context.ReadValue<Vector2>();
+            IsMove = inputMove != Vector2.zero;
 
+            SetFacingDirection(inputMove);
         }
-        
 
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            if (context.started)        //입력값이 있으면
+            {
+                IsRun = true;
+            }
+            else if (context.canceled)  //입력이 종료되는 순간
+            {
+                IsRun = false;
+            }
+        }
 
     }
 }
